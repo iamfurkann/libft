a440557d06dac4a7b56fa6866ae0bf38a59856e8
iamfurkann <esadfurkanduman@gmail.com>
2025-06-02 06:13:06 +0300

    updated ft_strjoin.c with solid type

--- reallibft/ft_strjoin.c
+++ reallibft/ft_strjoin.c
@@ -6,33 +6,43 @@
 /*   By: esduman <esduman@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 14:56:44 by esduman           #+#    #+#             */
-/*   Updated: 2025/05/31 17:09:12 by esduman          ###   ########.fr       */
+/*   Updated: 2025/06/02 06:11:44 by esduman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	f_add(char const *s, char *new_arr, size_t *i)
+{
+	size_t	j;
+
+	j = 0;
+	while (s[j])
+	{	
+		new_arr[*i] = s[j];
+		(*i)++;
+		j++;
+	}
+}
+
+int	f_strsum(char const *s1, char const *s2)
+{
+	return (ft_strlen((char *)s1) + ft_strlen((char *)s2));
+}
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*new_arr;
-	size_t	s1len;
-	size_t	s2len;
 	size_t	i;
-	size_t	j;
 
 	if (!s1 || !s2)
 		return (NULL);
-	s1len = ft_strlen((char *)s1);
-	s2len = ft_strlen((char *)s2);
-	new_arr = (char *)malloc((s1len + s2len) + 1);
+	new_arr = (char *)malloc(f_strsum(s1, s2) + 1);
 	if (!new_arr)
 		return (NULL);
-	i = -1;
-	while (++i < s1len)
-		new_arr[i] = s1[i++];
-	j = -1;
-	while (++j < s2len)
-		new_arr[i + j] = s2[j];
-	new_arr[i + j] = '\0';
+	i = 0;
+	f_add(s1, new_arr, &i);
+	f_add(s2, new_arr, &i);
+	new_arr[i] = '\0';
 	return (new_arr);
 }
