4f939f1ac7a5b9f950308603602083b0088846b4
iamfurkann <esadfurkanduman@gmail.com>
2025-06-02 09:01:02 +0300

    updated ft_substr.c with solid type and updated include types in ft_memset.c and ft_memmove.c

--- reallibft/ft_substr.c
+++ reallibft/ft_substr.c
@@ -6,32 +6,41 @@
 /*   By: esduman <esduman@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 14:05:20 by esduman           #+#    #+#             */
-/*   Updated: 2025/05/31 16:23:54 by esduman          ###   ########.fr       */
+/*   Updated: 2025/06/02 08:59:42 by esduman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	f_isbig(size_t *len, size_t length, unsigned int start)
+{
+	if (*len > length - start)
+		*len = length - start;
+}
+
+static char	*f_get_null(void)
+{
+	char	*new_value;
+
+	new_value = (char *)ft_calloc(1, 1);
+	if (!new_value)
+		return (NULL);
+	return (new_value);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*new_value;
-	size_t	length;
 
-	length = strlen(s);
-	if (start >= length)
-	{
-		new_value = (char *)ft_calloc(1, 1);
-		if (!new_value)
-			return (NULL);
-		return (new_value);
-	}
-	if (len > length - start)
-		len = length - start;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen((char *)s))
+		return (f_get_null());
+	f_isbig(&len, ft_strlen((char *)s), start);
 	new_value = (char *)malloc(len + 1);
 	if (!new_value)
 		return (NULL);
-	while (len-- && *(char *)(s + start))
-		*new_value++ = *(s + start++);
-	*new_value = '\0';
+	ft_memmove(new_value, s + start, len);
+	new_value[len] = '\0';
 	return (new_value);
 }
